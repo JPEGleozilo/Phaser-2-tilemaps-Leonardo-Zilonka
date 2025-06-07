4d096119cb694ab7535de2fbb14d4d0d4c2b714c
JPEGleozilo <zilonkaleo@gmail.com>
2025-06-07 07:42:47 -0300

    Nivel 3 y cámara

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Nivel1 from "./scenes/Nivel1.js";
 import Nivel2 from "./scenes/Nivel2.js";
+import Nivel3 from "./scenes/Nivel3.js";
 import victoria from "./scenes/victoria.js"
 
 // Create a new Phaser config object
@@ -19,6 +20,10 @@ const config = {
       height: 1200,
     },
   },
+  render: {
+    pixelArt: true,
+    roundPixels: true
+  },
   physics: {
     default: "arcade",
     arcade: {
@@ -29,7 +34,7 @@ const config = {
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [Nivel1, Nivel2, victoria],
+  scene: [Nivel1, Nivel2, Nivel3, victoria],
 };
 
 // Create a new Phaser game instance
--- scenes/Nivel3.js
+++ scenes/Nivel3.js
@@ -1,6 +1,6 @@
-export default class Nivel2 extends Phaser.Scene {
+export default class Nivel3 extends Phaser.Scene {
   constructor() {
-    super("Nivel2");
+    super("Nivel3");
   }
 
   init(data){
@@ -9,7 +9,7 @@ export default class Nivel2 extends Phaser.Scene {
   }
 
   preload() {
-    this.load.tilemapTiledJSON("nivel2", "public/assets/tilemap/Nivel2.json");
+    this.load.tilemapTiledJSON("nivel3", "public/assets/tilemap/Nivel3.json");
     this.load.image("tilemap", "public/assets/tilemap/tilemap.png");
     this.load.image("objetivo", "public/assets/objective.png");
     this.load.spritesheet("player", "public/assets/player.png", {
@@ -19,7 +19,10 @@ export default class Nivel2 extends Phaser.Scene {
 }
 
 create() {
-    this.objetos = 6;
+
+    this.cameras.main.setOrigin(0.5, 0.5)
+
+    this.objetos = 8;
     this.recolectados = 0;
 
     this.restante = this.objetos - this.recolectados;
@@ -27,7 +30,7 @@ create() {
     this.textures.get('tilemap').setFilter(Phaser.Textures.FilterMode.NEAREST);
     this.textures.get('player').setFilter(Phaser.Textures.FilterMode.NEAREST);
     this.textures.get('objetivo').setFilter(Phaser.Textures.FilterMode.NEAREST);
-    const map = this.make.tilemap({ key: "nivel2" });
+    const map = this.make.tilemap({ key: "nivel3" });
     const tileset = map.addTilesetImage("tilemap", "tilemap");
     const fondo = map.createLayer("fondo", tileset, 0, 0);
     const paredes = map.createLayer("paredes", tileset, 0, 0);
@@ -54,7 +57,10 @@ create() {
     this.player.setScale(5);
     this.player.setOrigin(0.5, 0.5)
 
-    this.player.setCollideWorldBounds(true);
+    this.cameras.main.setBounds(0, 0, map.widthInPixels * 5, map.heightInPixels * 5);
+    this.cameras.main.startFollow(this.player, true, 0.1, 0.1);
+    this.cameras.main.roundPixels = true;
+
     paredes.setCollisionByExclusion([-1]);
     this.physics.add.collider(this.player, paredes);
     
@@ -83,7 +89,7 @@ create() {
         }
     });
 
-    this.tiempotexto = this.add.text(0.5 * this.scale.width, 20, `${this.tiempo}s`, {
+    this.tiempotexto = this.add.text(0.5 * this.cameras.main.width, 0.1 * this.cameras.main.height, `${this.tiempo}s`, {
         fontSize: '16px',
         fill: '#fff'
     }).setOrigin(0.5, 0.5);
@@ -101,6 +107,12 @@ create() {
 }
 
 update() {
+
+    this.tiempotexto.setPosition(0.5 * this.cameras.main.width, 0.05 * this.cameras.main.height, `${this.tiempo}s`, {
+    fontSize: '16px',
+    fill: '#fff'
+}).setOrigin(0.5, 0.5)
+  .setScrollFactor(0);
 
     if (this.textorestante) {
         this.textorestante.destroy();
@@ -134,9 +146,9 @@ update() {
             });
             this.textorestante.setOrigin(0.5, 0.5);
         } else if (this.restante <= 0) {
-            this.scene.start("Nivel3", { score: this.score, tiempo: this.tiempo });
+            this.scene.start("victoria", { score: this.score, tiempo: this.tiempo });
         }
     }
   }
-
+  
 }
